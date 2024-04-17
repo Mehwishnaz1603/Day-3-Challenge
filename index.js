b4d225bbf2afe45fae79f7ffa88985d94b71e5ec
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-04-18 00:20:45 +0500

    Day 3

---
+++ index.js
@@ -0,0 +1,18 @@
+//🚀 Day 3 Challenge: Start Coding! 🚀//
+//Question 7: Number Eight: Write addition, subtraction, multiplication, and division
+//operations that each result in the number 8. Enclose your operations in print statements.
+//Question 8: You should create four lines that look like this:
+//console.log(5 + 3)
+//Your output should simply be four lines with the number 8 appearing once on each line.
+//In Addition Case//
+console.log(5 + 3);
+//In Substract Case//
+console.log(11 - 3);
+//In Multiplication Case//
+console.log(4 * 2);
+//In Division Case//
+console.log(16 / 2);
+//Question 9: Favorite Number: Store your favorite number in a variable. Then, create a
+// message revealing your favorite number. Print that message.
+var favouriteNumber = 7;
+console.log("My favourite Number is ".concat(favouriteNumber));
